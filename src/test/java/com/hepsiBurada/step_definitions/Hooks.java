040a0cbc9689c6762bf809cf5545846f8207ad9d
Canan <katehudson0004@gmail.com>
2024-12-22 23:01:34 +0300

    Scenario 2 completed

---
+++ src/test/java/com/hepsiBurada/step_definitions/Hooks.java
@@ -0,0 +1,40 @@
+package com.hepsiBurada.step_definitions;
+
+import com.hepsiBurada.utilities.ConfigurationReader;
+import com.hepsiBurada.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import io.restassured.RestAssured;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.time.Duration;
+
+
+public class Hooks {
+
+
+    @Before("@UI")
+    public void setUp() {
+        Driver.get().get(ConfigurationReader.get("url"));
+        Driver.get().manage().window().maximize();
+        Driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @After("@UI")
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()) {
+            final byte[] screenshot = ((TakesScreenshot) Driver.get()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", "screenshot");
+        }
+
+//        Driver.closeDriver();
+    }
+
+    @Before("@api")
+    public void api() {
+        RestAssured.baseURI = ConfigurationReader.get("apiUrl");
+    }
+
+}
